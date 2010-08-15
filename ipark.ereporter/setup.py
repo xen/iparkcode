f134cd98c20e274c1446b21ca463c062c8e382d1
Mikhail Kashkin <mk@iparkcorp.com>
2010-08-15 21:29:28 +0300

    changed setup.py for  ipark.ereporter

--- ipark.ereporter/setup.py
+++ ipark.ereporter/setup.py
@@ -1,11 +1,44 @@
+"""
+ipark.ereporter
+===============
+
+Usefull tipfy extension that send developer every exeption traceback report by email. 
+Its very handy when you beta test your projects and dont sure if something is going wrong.
+
+Setup
+-------
+
+To use this extension in your application follow few simple steps:
+
+- Add to buildout.cfg egg import
+- Modify config.py and add folowing lines::
+
+  # List of installed apps
+  config['tipfy'] = {
+    'apps_installed': [
+      ...
+      'ipark.common.ereporter',
+      ...
+     ]
+   }
+  #
+  config['ipark.ereporter.ereporter'] = {
+     'email' : 'XXX@domain.com', # Use developer's email as it is free to send 
+  }
+
+
+"""
 from setuptools import setup
 
 setup(
     name = "ipark.ereporter",
     version = "0.2",
-    description = 'Send exceptions via mail',
-    author = 'Ilya Petrov',
-    author_email = 'petrov@iparkcorp.com',
+    description = 'Tipfy extension to simplify projects beta test, it simply send all exceptions traceback via'+\
+      ' email to application administrators',
+    long_description = __doc__,
+    zip_safe = False,
+    author = 'Ilya Petrov, Mikhail Kashkin',
+    author_email = 'mk@iparkcorp.com',
     url = 'http://code.google.com/p/iparkcode/',
     packages = ['ipark', 'ipark.ereporter', ],
     license = "BSD",
@@ -13,4 +46,14 @@ setup(
     install_requires = [
         'tipfy==0.6', 
     ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
