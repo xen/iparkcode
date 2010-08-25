09a6b37aa42e4037f499ccad3936eb190298c48a
Mikhail Kashkin <mk@iparkcorp.com>
2010-08-25 18:31:40 +0300

    small fixes to make better pypi docs page

--- ipark.ereporter/setup.py
+++ ipark.ereporter/setup.py
@@ -1,17 +1,17 @@
 """
 ipark.ereporter
-===============
+================
 
 Usefull tipfy extension that send developer every exeption traceback report by email. 
 Its very handy when you beta test your projects and dont sure if something is going wrong.
 
 Setup
--------
+------
 
 To use this extension in your application follow few simple steps:
 
-- Add to buildout.cfg egg import
-- Modify config.py and add folowing lines::
+* Add to buildout.cfg egg import
+* Modify config.py and add folowing lines::
 
   # List of installed apps
   config['tipfy'] = {
